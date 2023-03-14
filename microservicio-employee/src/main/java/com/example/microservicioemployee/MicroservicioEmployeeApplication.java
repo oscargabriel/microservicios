5c049acfb231ad6dde0cafa9a49cfeaa30786d48
oscargabriel <ozambranopinto@gmail.com>
2023-03-14 18:27:21 -0400

    añadiendo el proyecto de microservicio para empleado

---
+++ microservicio-employee/src/main/java/com/example/microservicioemployee/MicroservicioEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicioemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioEmployeeApplication.class, args);
+	}
+
+}
---
+++ microservicio-employee/src/test/java/com/example/microservicioemployee/MicroservicioEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicioemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
